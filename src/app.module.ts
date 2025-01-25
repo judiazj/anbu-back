d856405e90b77142203e5bb0634bf0cf64dccfb5
Judiazj <judiazj@unal.edu.co>
2025-01-24 23:04:05 -0500

    configuracion variables de entorno

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
-import { ShinobiModule } from './shinobi/shinobi.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
+import { ShinobiModule } from './shinobi/shinobi.module';
+import { envs } from './config';
+
 @Module({
-  imports: [ShinobiModule, MongooseModule.forRoot('')],
+  imports: [ShinobiModule, MongooseModule.forRoot(envs.mongoUri)],
   controllers: [],
   providers: [],
 })
